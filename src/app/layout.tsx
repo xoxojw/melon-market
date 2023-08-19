8db37f84fbc897d60741cf01141b600a6e12bd1e
Jiwon Park <xoxojw.dev@gmail.com>
2023-08-20 00:58:27 +0900

    :tada: Init the project

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import '@/styles/globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: '감자마켓',
+  description: 'NextJS 뿌수기',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="ko">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>Main Page</div>;
+};
+
+export default Home;
