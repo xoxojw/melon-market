558a743b42753efdc11945c7938f9a09d2088702
Jiwon Park <xoxojw.dev@gmail.com>
2023-08-28 23:01:22 +0900

    :lipstick: Update streams/create page UI

--- src/app/streams/create/page.tsx
+++ src/app/streams/create/page.tsx
@@ -1,27 +1,8 @@
-import type { NextPage } from "next";
+import { NextPage } from 'next';
 
-const Upload: NextPage = () => {
+const Create: NextPage = () => {
   return (
     <div className="space-y-5 px-4 py-8">
-      <div>
-          <label className="w-full h-60 py-6 flex items-center justify-center border-2 border-dashed border-gray-400 rounded-sm hover:text-melon-500 hover:border-melon-200 transition ease-in-out duration-200 cursor-pointer">
-            <svg
-              className="h-12 w-12"
-              stroke="currentColor"
-              fill="none"
-              viewBox="0 0 48 48"
-              aria-hidden="true"
-            >
-              <path
-                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-            <input className="hidden" type="file" />
-          </label>
-      </div>
       <div>
         <label
           className="mb-1 block text-sm font-medium text-gray-900"
@@ -56,10 +37,10 @@ const Upload: NextPage = () => {
       <button
         className="w-full mx-auto items-center mt-6 px-4 py-2 bg-melon-300 text-white border-transparent outline-melon-400 hover:bg-melon-400 focus:border-melon-400 rounded-md shadow-sm font-medium transition ease-in-out duration-200"
       >
-        업로드 하기
+        라이브 스트림 시청하기
       </button>
     </div>
-  );
-};
+  )
+}
 
-export default Upload;
+export default Create;
